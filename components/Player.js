29fc3a656006d96273f2af6383c7fba1e4fb692a
zeppytoh <victortoh@gmail.com>
2022-01-26 22:23:24 +0900

    Fix volume control

--- components/Player.js
+++ components/Player.js
@@ -1,12 +1,13 @@
 import { useSession } from "next-auth/react";
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useCallback } from 'react';
 import { useRecoilState } from "recoil";
 import { currentTrackIdState, isPlayingState } from "../atoms/songAtom";
 import useSongInfo from "../hooks/useSongInfo";
 import useSpotify from "../hooks/useSpotify";
-
-import { RewindIcon, ReplyIcon, FastForwardIcon, VolumeOffIcon, VolumeUpIcon } from "@heroicons/react/outline";
+import { debounce } from "lodash";
+import { RewindIcon, ReplyIcon, FastForwardIcon, VolumeUpIcon } from "@heroicons/react/outline";
 import { SwitchHorizontalIcon, PlayIcon, PauseIcon } from "@heroicons/react/solid";
+import { Volume1, Volume2 } from 'react-feather';
 
 function Player() {
   const spotifyApi = useSpotify();
@@ -48,6 +49,20 @@ function Player() {
       setVolume(50);
     }
   }, [currentTrackIdState, spotifyApi, session]);
+
+  useEffect(() => {
+    if (volume > 0 && volume < 100) {
+      debouncedAdjustVolume(volume);
+      console.log(volume);
+    }
+  }, [volume]);
+
+  const debouncedAdjustVolume = useCallback(
+    debounce((volume) => {
+      spotifyApi.setVolume(volume).catch((err) => { });
+    }, 300),
+    []
+  )
   
   return (
     <div className="h-24 bg-gradient-to-b from-black to-gray-800 text-white grid grid-cols-3 text-xs md:text-base px-2 md:px-8 border-t-2 border-slate-800">
@@ -72,9 +87,9 @@ function Player() {
         <ReplyIcon className="button" />
       </div>
       <div className="flex items-center space-x-3 md:space-x-4 justify-end pr-5">
-        <VolumeOffIcon className="button" />
-        <input type="range" value="" min={0} max={100} />
-        <VolumeUpIcon className="button" />
+        <Volume1 className="button" onClick={()=> volume > 0 && setVolume(volume - 10)} />
+        <input className="w-4 md:w-28" type="range" value={volume} onChange={e => setVolume(Number(e.target.value))} min={0} max={100} />
+        <Volume2 className="button" onClick={()=> volume < 100 && setVolume(volume + 10)} />
       </div>
     </div>
   )
